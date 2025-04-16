c7f1841f269c308ccafef6665c6890a042834bf5
idris10215 <mohammedidris10215@gmail.com>
2025-04-16 06:37:32 +0530

    Added Support component

--- frontend/src/landing_page/support/Hero.jsx
+++ frontend/src/landing_page/support/Hero.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <div
+        className="p-5"
+        style={{ backgroundColor: "#387ed1", color: "white" }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-7 mb-5">
+              <h4 className="mb-5">Support Portal</h4>
+              <h3 className="mb-4">
+                Search for an answer or browse help topics to create a ticket
+              </h3>
+              <input
+                type="text"
+                style={{ width: "90%", height: "3.5rem", padding: "1rem" }}
+                placeholder="Eg: how do i activate F&O, why is my order getting rejected ..."
+              ></input>
+              <br />
+              <div className="mt-3 fs-5">
+                <a
+                  href="https://zrd.sh/signup"
+                  style={{ color: "white", marginRight: "2rem" }}
+                  className="link-offset-3"
+                >
+                  Track account opening
+                </a>
+                <a
+                  href="https://zrd.sh/segment-activation"
+                  style={{ color: "white", marginRight: "2rem" }}
+                  className="link-offset-3"
+                >
+                  Track segment activation
+                </a>
+                <a
+                  href="https://zrd.sh/segment-activationF"
+                  style={{ color: "white", marginRight: "2rem" }}
+                  className="link-offset-3"
+                >
+                  Intraday margins
+                </a>
+                <br />
+                <div className="mt-3">
+                  <a
+                    href="https://zrd.sh/kite-manual"
+                    style={{ color: "white", marginRight: "2rem" }}
+                    className="link-offset-3 "
+                  >
+                    Kite user manual
+                  </a>
+                </div>
+              </div>
+            </div>
 
-export default Hero
+            <div className="col-5">
+              <div
+                className=" row mb-5 pb-3 fs-5"
+                style={{ marginLeft: "70%" }}
+              >
+                <a
+                  href="https://support.zerodha.com/login?next=https%3A%2F%2Fsupport.zerodha.com%2Ftickets%2F"
+                  style={{ color: "white" }}
+                  className="link-offset-3"
+                >
+                  Track tickets
+                </a>
+              </div>
+              <h3>Featured</h3>
+              <ol>
+                <li className="mt-3">
+                  <a
+                    href="Trading holiday on account of Dr. Baba Saheb Ambedkar Jayanti on April 14, 2025"
+                    style={{ color: "white" }}
+                    className="link-offset-3 fs-5"
+                  >
+                    Change in expiry day of NSE derivative contracts from April
+                    04, 2025 [Withheld]
+                  </a>
+                </li>
+                <li className="mt-3">
+                  <a
+                    href="https://zerodha.com/marketintel/bulletin/410695/trading-holiday-on-account-of-dr-baba-saheb-ambedkar-jayanti-on-april-14-2025"
+                    style={{ color: "white" }}
+                    className="link-offset-3 fs-5"
+                  >
+                    Trading holiday on account of Dr. Baba Saheb Ambedkar
+                    Jayanti on April 14, 2025
+                  </a>
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
--- frontend/src/landing_page/support/RaiseTicket.jsx
+++ frontend/src/landing_page/support/RaiseTicket.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
 
 const RaiseTicket = () => {
   return (
-    <div>RaiseTicket</div>
-  )
-}
+    <>
+      <div className="container p-5  mt-4 ">
+        <h3 className="text-muted mb-5 font-color">
+          To create a ticket, select a relevant topic
+        </h3>
 
-export default RaiseTicket
+        <div className="row mb-5 ">
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/account-opening"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Account Opening
+            </a>
+            <p>Resident individual</p>
+            <p>Minor</p>
+            <p>Non Resident Indian (NRI)</p>
+            <p>Company, Partnership, HUF and LLP</p>
+            <p>Glossary</p>
+          </div>
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/your-zerodha-account"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Your Zerodha Account
+            </a>
+            <p>Your Profile</p>
+            <p>Account modification</p>
+            <p>Client Master Report (CMR) and Depository Participant (DP)</p>
+            <p>Nomination</p>
+            <p>Transfer and conversion of securities</p>
+          </div>
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/trading-and-markets"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Kite
+            </a>
+            <p>IPO</p>
+            <p>Trading FAQs</p>
+            <p>Margin Trading Facility (MTF) and Margins</p>
+            <p>Charts and orders</p>
+            <p>Alerts and Nudges</p>
+            <p>General</p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/funds"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Funds
+            </a>
+            <p>Add money</p>
+            <p>Withdraw money</p>
+            <p>Add bank accounts</p>
+            <p>eMandates</p>
+          </div>
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/console"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Console
+            </a>
+            <p>Portfolio</p>
+            <p>Corporate actions</p>
+            <p>Funds statement</p>
+            <p>Reports</p>
+            <p>Profile</p>
+            <p>Segments</p>
+          </div>
+          <div className="col">
+            <a
+              href="https://support.zerodha.com/category/mutual-funds"
+              className="d-block mb-4 "
+              style={{ textDecoration: "none", fontSize: "1.5rem" }}
+            >
+              Coin
+            </a>
+            <p>Understanding mutual funds and Coin</p>
+            <p>Coin app</p>
+            <p>Coin web</p>
+            <p>Transactions and reports</p>
+            <p>National Pension Scheme (NPS)</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RaiseTicket;
